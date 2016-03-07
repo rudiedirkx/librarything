e3cf72b7c65503fef310c177d232e75b5290f335
Rudie Dirkx <github@hotblocks.nl>
2016-03-07 22:57:07 +0100

    guzzle+, -old

--- guzzlelogin.php
+++ guzzlelogin.php
@@ -1,14 +1,17 @@
 <?php
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\FileCookieJar;
 use GuzzleHttp\RedirectMiddleware;
 
 require 'inc.bootstrap.php';
 
 header('Content-type: text/plain; charset=utf8');
 
+$cookies = new FileCookieJar('/tmp/librarything-guzzle-2.cookies', true);
+
 $client = new Client([
-	'cookies' => true,
+	'cookies' => $cookies,
 	'allow_redirects' => array(
 		'track_redirects' => true,
 	) + RedirectMiddleware::$defaultSettings,
@@ -16,27 +19,22 @@ $client = new Client([
 
 // GET /
 $res = $client->request('GET', LT_BASE_URL . '/', []);
-var_dump($res->getStatusCode());
-// print_r($res->getHeaders());
 
-// POST /enter/start
-// GET /enter/checkcookies/2403928250
-// GET /enter/process/signinform
-// GET /home
-// GET /
-$res = $client->request('POST', LT_BASE_URL . '/enter/start', [
-	'form_params' => array(
-		'formusername' => LT_USER_NAME,
-		'formpassword' => LT_USER_PASS,
-		'index_signin_already' => 'Sign in',
-	),
-]);
-var_dump($res->getStatusCode());
-// print_r($res->getHeaders());
-// print_r($res);
-// echo $res->getBody();
+if ( strpos($res->getBody(), 'formusername') ) {
+	// POST /enter/start
+	// GET /enter/checkcookies/2403928250
+	// GET /enter/process/signinform
+	// GET /home
+	// GET /
+	$res = $client->request('POST', LT_BASE_URL . '/enter/start', [
+		'form_params' => array(
+			'formusername' => LT_USER_NAME,
+			'formpassword' => LT_USER_PASS,
+			'index_signin_already' => 'Sign in',
+		),
+	]);
+}
 
 // GET /catalog/rudiedirkx
 $res = $client->request('GET', LT_BASE_URL . '/', []);
-var_dump($res->getStatusCode());
 echo $res->getBody();
