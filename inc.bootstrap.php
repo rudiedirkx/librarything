00887ab8737fa5ddd2b0d235b1683073c365996b
Rudie Dirkx <github@hotblocks.nl>
2016-11-21 00:11:05 +0100

    +CSRF check

--- inc.bootstrap.php
+++ inc.bootstrap.php
@@ -11,6 +11,10 @@ require 'vendor/autoload.php';
 
 header('Content-type: text/html; charset=utf-8');
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && @parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) !== $_SERVER['HTTP_HOST']) {
+	exit("CSRF error");
+}
+
 $client = new Client(
 	new WebAuth(APP_COOKIE_FILE, LT_USER_NAME, LT_USER_PASS),
 	new FileCache(APP_CACHE_DIR, APP_CACHE_TTL)
